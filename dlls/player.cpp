73c488d40724fe21583ddd13c9afbd2ba523f0bf
ScriptedSnark <sevcenkobogdan855@gmail.com>
2021-12-13 16:50:33 +0500

    Fixed server crash when one player use tank that is used by other player doesn't have a weapon

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -995,7 +995,6 @@ void CBasePlayer::Killed( entvars_t *pevAttacker, int iGib )
 	if ( m_pTank != NULL )
 	{
 		m_pTank->Use( this, this, USE_OFF, 0 );
-		m_pTank = NULL;
 	}
 
 	// this client isn't going to be thinking for a while, so reset the sound until they respawn
@@ -1641,7 +1640,6 @@ void CBasePlayer::PlayerUse ( void )
 			// Stop controlling the tank
 			// TODO: Send HUD Update
 			m_pTank->Use( this, this, USE_OFF, 0 );
-			m_pTank = NULL;
 			return;
 		}
 		else
@@ -2812,7 +2810,6 @@ void CBasePlayer::PostThink()
 		else
 		{  // they've moved off the platform
 			m_pTank->Use( this, this, USE_OFF, 0 );
-			m_pTank = NULL;
 		}
 	}
 
